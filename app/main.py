e4b7d900d4a7aed52aa7f4745a6f1e1d8021254c
Abhishek-surya <abhisheksuryavanshi220@gmail.com>
2025-11-28 18:12:24 +0530

    Updated main

--- app/main.py
+++ app/main.py
@@ -2,9 +2,20 @@ from fastapi import FastAPI
 from app.core.database import Base, engine
 from app.controllers.job_controller import router as job_router
 
+import threading
+from app.worker.worker import run_worker
+from app.services.job_service import reset_running_jobs_on_restart
+
 app = FastAPI(title="Ticket Queue")
 
 Base.metadata.create_all(bind=engine)
+
+@app.on_event("startup")
+def startup_event():
+    db = next(get_db())
+    reset_running_jobs_on_restart(db)
+    worker_thread = threading.Thread(target=run_worker, daemon=True)
+    worker_thread.start()
 
 app.include_router(job_router) 
 
